a1f7ce52415839cb38b316c8381c8a38c4f4e94e
AmaryllisLee <ama.lee@student.hu.nl>
2021-05-17 13:37:33 +0200

    add getNextNode method

--- src/finite_state_machine/Node.java
+++ src/finite_state_machine/Node.java
@@ -3,8 +3,8 @@ package finite_state_machine;
 import java.util.ArrayList;
 
 public class Node {
-    String name; // defining the states
-    ArrayList<Transition> transitions = new ArrayList<>();
+    private String name; // defining the states
+    private ArrayList<Transition> transitions = new ArrayList<>();
 
     public Node(String name){
         this.name =name;
@@ -12,8 +12,15 @@ public class Node {
 
     public void setName(String name){this.name = name;}
     public String getName(){return name;}
-    public void addtransition(Transition transition){
-        transitions.add(transition);
+    public void addtransition(Transition transition){ transitions.add(transition); }
+
+    public Node getNextNode(Character value){
+        for (int j= 0; j< transitions.size(); j++){
+            if(transitions.get(j).getValue() == value){
+                return transitions.get(j).getConnectedNode();
+            }
+        }
+        return null;
     }
 
 }
